9b124eeaeeee8c74cfbcdfe725b5e405bb64162a
Lavrud <jr_durval@hotmail.com>
2020-05-08 17:35:46 -0300

    add homepage-design

--- src/js/libs/all.js
+++ src/js/libs/all.js
@@ -2,21 +2,3 @@
 $('.nav-pills a').click(function () {
     $(this).tab('show');
 })
-
-// vertical mode
-$(document).ready(function() {
-  $('#imageGallery').lightSlider({
-      gallery:true,
-      item:1,
-      loop:true,
-      thumbItem:9,
-      slideMargin:0,
-      enableDrag: false,
-      currentPagerPosition:'left',
-      onSliderLoad: function(el) {
-          el.lightGallery({
-              selector: '#imageGallery .lslide'
-          });
-      }   
-  });  
-});
